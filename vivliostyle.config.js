8e6a7ef6c65e85e1401aa9edcedb2413130cac54
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-08-23 01:13:06 +0900

    chore(vivliostyle): add output of output.pdf file and ./book directory

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -18,13 +18,14 @@ module.exports = {
     // 'glossary.html', // html can be passed.
   ], // `entry` can be `string` or `object` if there's only single markdown file.
   entryContext: "./manuscripts", // default to '.' (relative to `vivliostyle.config.js`).
-  // output: [ // path to generate draft file(s). default to '{title}.pdf'
-  //   './output.pdf', // the output format will be inferred from the name.
-  //   {
-  //     path: './book',
-  //     format: 'webpub',
-  //   },
-  // ],
+  output: [
+    // path to generate draft file(s). default to '{title}.pdf'
+    "./output.pdf", // the output format will be inferred from the name.
+    {
+      path: "./book",
+      format: "webpub",
+    },
+  ],
   // workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
