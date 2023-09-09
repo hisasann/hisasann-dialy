21750a1fed1c36ab89a030d3c44b1493d61921e9
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-09-10 03:27:59 +0900

    feat: 一旦この時点でvivliostyleはやめてInDesignにシフトする

---
+++ manuscripts/cover.md
@@ -0,0 +1,12 @@
+# @hisasann日記
+
+<img src="./cover.jpg" alt="hisasann" title="hisasann" class="cover">
+
+<div class="author">
+
+- 2023/12/10
+- 玉葱堂書店
+- 久松 由幸
+- @hisasann
+
+</div>
--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -3,14 +3,22 @@ module.exports = {
   author: "hisasann <hisasann25@gmail.com>", // default to `author` in `package.json` or undefined.
   language: "ja", // default to undefined.
   size: "A5", // paper size.
-  theme: "@vivliostyle/theme-bunko@beta", // .css or local dir or npm package. default to undefined.
+  theme: "@vivliostyle/theme-bunko", // .css or local dir or npm package. default to undefined.
   entry: [
+    "cover.md",
+    {
+      path: 'toc.md',
+      rel: 'contents',
+    },
     "prologue.md",
     "2021-08.md",
     "2021-09.md",
     "2021-10.md",
     "2021-11.md",
     "2021-12.md",
+    "2022-01.md",
+    "2022-02.md",
+    "2022-03.md",
     "epilogue.md",
     // {
     //   path: 'epigraph.md',
@@ -30,7 +38,8 @@ module.exports = {
   ],
   workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
   toc: true, // 目次を作成するかどうか
-  // cover: './cover.png', // cover image. default to undefined.
+  tocTitle: 'もくじ',
+  cover: './cover.jpg', // cover image. default to undefined.
   // vfm: { // options of VFM processor
   //   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
   //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
